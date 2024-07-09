2c97b70ea0f83797400a467371654bb3f12c1344
Seckyar <xero.mz93@gmail.com>
2024-07-09 18:54:05 +0630

    just fixing

--- javascript/script.js
+++ javascript/script.js
@@ -5,20 +5,6 @@ function goBack() {
     window.history.back();
 }
 
-function addFavorite() {
-    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    const params = new URLSearchParams(window.location.search);
-    const title = params.get('title');
-    console.log(title);
-    if (!favorites.includes(title)) {
-        favorites.push(title);
-        localStorage.setItem('favorites', JSON.stringify(favorites));
-        alert('Added to favorites');
-    } else {
-        alert('Already in favorites');
-    }
-}
-
 function removeFromFavorites(artist, title) {
     let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
     favorites = favorites.filter(fav => !(fav.artist === artist && fav.title === title));
